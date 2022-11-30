4f941a62f1cb7664c5c498ff054a3a7cd3c7a5ca
Jason <jason.onggo@tempatkerja.com>
2022-11-30 12:26:52 +0700

    add pipeline with sklearn, we will add tensorflow pipeline after feature selection

--- utils/num_imp.py
+++ utils/num_imp.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 def imp(df, **kwargs):
     df_c = df.copy()
     df_c.loc[((df_c['TotalCharges'].isna()) | (df_c['TotalCharges'] == ' ')) &
@@ -6,3 +9,8 @@ def imp(df, **kwargs):
 
 def imp_out(self, input_features):
     return self.kw_args['features']
+
+def sqrt(df, **kwargs):
+    df_c = pd.DataFrame(data=df, columns=kwargs['features'])
+    df_c['TotalCharges'] = np.sqrt(df_c['TotalCharges'])
+    return df_c
